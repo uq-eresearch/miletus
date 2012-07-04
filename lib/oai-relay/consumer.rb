f1133e62e047249378efe5e492e3a4eca6cf1783
Tim Dettrick <t.dettrick@uq.edu.au>
2012-07-04 10:06:03 +1000

    Making some behaviour changes to fit better with ruby-oai.

--- lib/oai-relay/consumer.rb
+++ lib/oai-relay/consumer.rb
@@ -1,28 +1,22 @@
 require 'oai'
 
 class Consumer
-  
-  module DataMixin
-    def data
-      @client.get_record(:metadata_format => @collection.format)
-    end
-  end
-  
+
   # Take endpoint to use and collection to update
   def initialize(endpoint, recordCollection)
     is_client = [:get_record, :list_identifiers].all? do |method|
       endpoint.respond_to?(method)
     end
-    
+
     is_collection = [:get, :add, :remove].all? do |method|
       recordCollection.respond_to?(method)
     end
-    
+
     unless is_collection
       raise ArgumentError.new("Consumer requires a collection to update.")
     end
-    @collection = recordCollection 
-    
+    @collection = recordCollection
+
     if is_client
       @client = endpoint
     else
@@ -33,15 +27,19 @@ class Consumer
       end
     end
   end
-  
+
+  # Update collection with changed records
   def update
     @client.list_identifiers(:metadataPrefix => @collection.format)\
-      .select { |record|
-        existing = @collection.get(record.identifier)
-        existing == nil or existing.datestamp < record.datestamp
-      }.each { |record|
-        @collection.add(record.extend(DataMixin))
+      .select { |header|
+        existing = @collection.get(header.identifier)
+        existing == nil or existing.datestamp < header.datestamp
+      }.each { |header|
+        record = @client.get_record(
+            :identifier => header.identifier,
+            :metadataPrefix => @collection.format).record
+        @collection.add(record)
       }
   end
-  
+
 end
--- spec/consumer_spec.rb
+++ spec/consumer_spec.rb
@@ -3,8 +3,24 @@ require 'yaml'
 
 describe Consumer do
 
-  def get_fixture_records(filename)
-    File.open(File.join(File.dirname(__FILE__), filename)) do |f| 
+  def get_record_response_for_header_lambda(filename)
+    # Convenience function for lambda
+    def get_record_for_header(headers, identifier)
+      headers.select { |header| header.identifier = identifier }\
+        .map { |header| Struct.new(:header, :metadata).new(header, '<xml/>') }\
+        .first
+    end
+    # Produce lambda
+    lambda do |h|
+      Struct.new(:record).new(
+        get_record_for_header(
+          get_fixture_objects(filename),
+          h[:identifier]))
+    end
+  end
+
+  def get_fixture_objects(filename)
+    File.open(File.join(File.dirname(__FILE__), filename)) do |f|
       YAML::load(f.read()).map do |h|
         # Convert hashes to structs which resemble OAI::Header
         k,v = h.to_a.transpose
@@ -12,16 +28,16 @@ describe Consumer do
       end
     end
   end
-    
+
   it "takes an OAI-PMH client and RecordCollection for initialization" do
     # Need an argument
     lambda { Consumer.new() }.should raise_error(ArgumentError)
-    
+
     # Not just any arguments
-    lambda { 
-      Consumer.new(Object.new(), Object.new()) 
+    lambda {
+      Consumer.new(Object.new(), Object.new())
     }.should raise_error(ArgumentError)
-    
+
     # OAI::Client and RecordCollection should be duck-typed
     client = double("OAI::Client")
     client.stub(:get_record)
@@ -31,29 +47,30 @@ describe Consumer do
     recordCollection.stub(:get)
     recordCollection.stub(:add)
     recordCollection.stub(:remove)
-    
-    Consumer.new(client, recordCollection) 
+
+    Consumer.new(client, recordCollection)
 
     # Check both arguments
-    lambda { 
-      Consumer.new(client, Object.new()) 
+    lambda {
+      Consumer.new(client, Object.new())
     }.should raise_error(ArgumentError)
-    lambda { 
-      Consumer.new(Object.new(), recordCollection) 
+    lambda {
+      Consumer.new(Object.new(), recordCollection)
     }.should raise_error(ArgumentError)
-    
+
     # A string will also do
     Consumer.new("http://example.test/oai", recordCollection)
   end
-  
+
   it "adds all records for an empty collection" do
     # OAI::Client and RecordCollection should be duck-typed
     client = double("OAI::Client")
     client.should_receive(:list_identifiers).with(:metadataPrefix => 'rif')\
-      .and_return(get_fixture_records('fixtures/list_identifiers_1.yaml'))
-    # This should be lazily called
-    client.should_not_receive(:get_record)
-    
+      .and_return(get_fixture_objects('fixtures/list_identifiers_1.yaml'))
+    # This should be called for all records
+    client.should_receive(:get_record).exactly(8).times.and_return\
+      &get_record_response_for_header_lambda('fixtures/list_identifiers_1.yaml')
+
     recordCollection = double("RecordCollection")
     # Record collection should have its format checked
     recordCollection.should_receive(:format).at_least(:once).and_return('rif')
@@ -61,25 +78,27 @@ describe Consumer do
     recordCollection.should_receive(:get).with(%r{^http://example.test/})\
                     .exactly(8).times.and_return(nil)
     # All records should be added
-    recordCollection.should_receive(:add).with(duck_type(:identifier, :data))\
+    recordCollection.should_receive(:add)\
+                    .with(duck_type(:header, :metadata))\
                     .exactly(8).times
     # No records should be removed
     recordCollection.should_not_receive(:remove)
-    
-    consumer = Consumer.new(client, recordCollection) 
+
+    consumer = Consumer.new(client, recordCollection)
     consumer.update()
   end
-  
+
   it "updates records with different datetimes for an existing collection" do
-    existing_records = get_fixture_records('fixtures/list_identifiers_1.yaml')
-    
+    existing_records = get_fixture_objects('fixtures/list_identifiers_1.yaml')
+
     # OAI::Client and RecordCollection should be duck-typed
     client = double("OAI::Client")
     client.should_receive(:list_identifiers).with(:metadataPrefix => 'rif')\
-      .and_return(get_fixture_records('fixtures/list_identifiers_2.yaml'))
-    # This should be lazily called
-    client.should_not_receive(:get_record)
-    
+      .and_return(get_fixture_objects('fixtures/list_identifiers_2.yaml'))
+    # One new record => one call
+    client.should_receive(:get_record).exactly(1).times.and_return\
+      &get_record_response_for_header_lambda('fixtures/list_identifiers_2.yaml')
+
     recordCollection = double("RecordCollection")
     # Record collection should have its format checked
     recordCollection.should_receive(:format).at_least(:once).and_return('rif')
@@ -89,13 +108,14 @@ describe Consumer do
         existing_records.select { |r| q == r.identifier }.first
       }
     # All records should be added
-    recordCollection.should_receive(:add).with(duck_type(:identifier, :data))\
+    recordCollection.should_receive(:add)\
+                    .with(duck_type(:header, :metadata))\
                     .exactly(1).times
     # No records should be removed
     recordCollection.should_not_receive(:remove)
-    
-    consumer = Consumer.new(client, recordCollection) 
+
+    consumer = Consumer.new(client, recordCollection)
     consumer.update()
   end
-  
+
 end
