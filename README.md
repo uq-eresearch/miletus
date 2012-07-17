926a5e8c097831ca177f3783e8e8c4bea1f05ae0
Tim Dettrick <t.dettrick@uq.edu.au>
2012-07-17 17:01:24 +1000

    Shifting monkey patch so we can use rails commands.

--- README.md
+++ README.md
@@ -11,12 +11,16 @@ _More to come later..._
 
 The database connection is provided by the `DATABASE_URL` environment variable.
 
-For running Rake tasks adhoc:
-
-    DATABASE_URL="postgres:///my_db" rake jobs:work
-
-Or to run the whole lot, you can take advantage of Foreman's `.env` file:
+Take advantage of Foreman's `.env` file:
 
     echo "DATABASE_URL=postgres:///my_db" > .env
+
+To run Rake tasks adhoc:
+
+    foreman run rake jobs:work
+
+Or to run the whole lot:
+
     foreman start
+
 	
--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,48 @@ if defined?(Bundler)
   # Bundler.require(:default, :assets, Rails.env)
 end
 
+# Monkey-patch Rails to handle the lack of database.yml
+class Rails::Application::Configuration
+
+  def database_configuration
+    # There is no config file, so manufacture one
+    config = {
+      'test' => 'sqlite3://localhost/:memory:',
+      'development' => ENV['DATABASE_URL'],
+      'production' => ENV['DATABASE_URL']
+    }
+    config.each_key do |key|
+      # Based on how Heroku do it: https://gist.github.com/1059446
+      begin
+        uri = URI.parse(config[key])
+
+        # Values
+        adapter = uri.scheme
+        adapter = "postgresql" if adapter == "postgres"
+        database = (uri.path || "").split("/")[1]
+        username = uri.user
+        password = uri.password
+        host = uri.host
+        port = uri.port
+
+        config[key] = {
+          'adapter' => adapter,
+          'database' => database,
+          'username' => username,
+          'password' => password,
+          'host' => host,
+          'port' => port
+        }
+      rescue URI::InvalidURIError
+        config.delete(key)
+      end
+    end
+
+    config
+  end
+
+end
+
 module Miletus
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
