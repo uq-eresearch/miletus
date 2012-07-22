22842f333f533f9c4519a8cd8575fe8b5189dc4c
Tim Dettrick <t.dettrick@uq.edu.au>
2012-07-23 09:57:28 +1000

    Altering output record creation to be on a delay.

--- app/models/rifcs_record_observer.rb
+++ app/models/rifcs_record_observer.rb
@@ -6,9 +6,7 @@ class RifcsRecordObserver < ActiveRecord::Observer
   observe Miletus::Harvest::OAIPMH::RIFCS::Record
 
   def after_create(record)
-    Miletus::Output::OAIPMH::Record.new(
-      :metadata => record.to_rif
-    ).save!
+    delay.process_change(:create, record)
   end
 
   def after_update(record)
@@ -18,5 +16,12 @@ class RifcsRecordObserver < ActiveRecord::Observer
   def after_destroy(record)
     # TODO: Implement
   end
+
+  def process_change(action, record)
+    Miletus::Output::OAIPMH::Record.new(
+      :metadata => record.to_rif
+    ).save!
+  end
+
 
 end
--- spec/models/rifcs_record_observer_spec.rb
+++ spec/models/rifcs_record_observer_spec.rb
@@ -10,9 +10,13 @@ describe RifcsRecordObserver do
   it { should respond_to(:after_create, :after_update, :after_destroy) }
 
   it "should create a new output record for a new harvested record" do
+    # New record should have been created on a delay, not immediately
+    subject.should_receive(:delay).at_least(1).and_return(subject)
+    # Load data from fixture
     fixture_file = File.join(File.dirname(__FILE__),
       '..', 'fixtures','rifcs-party-1.xml')
     xml = File.open(fixture_file) { |f| f.read }
+    # Create record
     input_record = Miletus::Harvest::OAIPMH::RIFCS::Record.new.tap do |r|
       r.identifier = 'http://example.test/1'
       r.datestamp = Time.now
@@ -22,6 +26,7 @@ describe RifcsRecordObserver do
     end
     # Run hook
     subject.after_create(input_record)
+    # A new record should exist as a result
     output_record = Miletus::Output::OAIPMH::Record.find(:first)
     output_record.should_not be(nil)
     output_record.to_rif.should_not be(nil)
