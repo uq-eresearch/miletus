acde362d27787549298a11a7db2ae3c564bd82ea
Tim Dettrick <t.dettrick@uq.edu.au>
2012-08-01 15:13:11 +1000

    Adding tests for updating and deleting output records.

--- spec/models/oaipmh_output_observer_spec.rb
+++ spec/models/oaipmh_output_observer_spec.rb
@@ -36,10 +36,59 @@ describe OaipmhOutputObserver do
     concept = create_concept
     # Run hook - which will happen as part of the environment
     # subject.after_create(input_record)
-    # A new concept should exist as a result
+    # A new record should exist as a result
     output_record = Miletus::Output::OAIPMH::Record.find(:first)
     output_record.should_not be(nil)
     output_record.to_rif.should_not be(nil)
+  end
+
+  it "should update an existing output record when a concept updates" do
+    # Disable delayed run for hooks
+    RifcsRecordObserver.stub(:run_job).and_return { |j| j.run }
+    concept = create_concept
+    # Run hook - which will happen as part of the environment
+    # subject.after_create(input_record)
+    # A new record should exist as a result
+    output_record = Miletus::Output::OAIPMH::Record.find(:first)
+    output_record.should_not be(nil)
+    output_record.to_rif.should_not be(nil)
+    # Change facet
+    facet = concept.facets.first
+    doc = Nokogiri::XML(facet.metadata)
+    nodes = doc.xpath("//rif:namePart[@type='given'][text()='John']",
+      'rif' => 'http://ands.org.au/standards/rif-cs/registryObjects')
+    nodes.each do |e|
+      e.remove
+    end
+    facet.metadata = doc
+    facet.save!
+    # The concept should update as a result
+    output_record = Miletus::Output::OAIPMH::Record.find(:first)
+    output_record.should_not be_nil
+    output_record.to_rif.should_not be_nil
+    rifcs_doc = Nokogiri::XML::Document.parse(output_record.to_rif)
+    rifcs_doc.xpath("//rif:namePart[@type='given'][text()='John']",
+      'rif' => 'http://ands.org.au/standards/rif-cs/registryObjects')\
+       .should be_empty
+  end
+
+  it "should mark output records as deleted when concepts have no entries" do
+    # Disable delayed run for hooks
+    RifcsRecordObserver.stub(:run_job).and_return { |j| j.run }
+    concept = create_concept
+    # Run hook - which will happen as part of the environment
+    # subject.after_create(input_record)
+    # A new record should exist as a result
+    output_record = Miletus::Output::OAIPMH::Record.find(:first)
+    output_record.should_not be(nil)
+    output_record.to_rif.should_not be(nil)
+    # Delete facet
+    concept.facets.first.destroy
+    Miletus::Merge::Facet.all.count.should == 0
+    # The record should be marked deleted as a result
+    output_record = Miletus::Output::OAIPMH::Record.find(:first)
+    output_record.should_not be_nil
+    output_record.should be_deleted
   end
 
 
