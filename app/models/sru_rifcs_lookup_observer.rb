6c429ffe7d24f2296333762824a0601b3261391e
Tim Dettrick <t.dettrick@uq.edu.au>
2012-09-13 15:23:59 +1000

    Refactoring SRU lookup observer while ensuring it properly blocks loops.

--- app/models/sru_rifcs_lookup_observer.rb
+++ app/models/sru_rifcs_lookup_observer.rb
@@ -7,7 +7,12 @@ class SruRifcsLookupObserver < ActiveRecord::Observer
   observe Miletus::Merge::Facet
 
   def find_sru_records(concept_or_facet)
-    concept = concept_or_facet.concept rescue concept_or_facet
+    concept = concept_or_facet
+    if concept_or_facet.respond_to?(:concept)
+      facet = concept_or_facet
+      return if blocked_key?(facet.key)
+      concept = facet.concept
+    end
     Miletus::Harvest::SRU::Interface.find(:all).each do |interface|
       self.class.run_job(JobProcessor.new(concept, interface))
     end
@@ -15,12 +20,27 @@ class SruRifcsLookupObserver < ActiveRecord::Observer
 
   alias :after_save :find_sru_records
 
+  def blocked_key?(key)
+    @blocked_keys ||= []
+    @blocked_keys.include?(key)
+  end
+
+  def prevent_loop(key, &block)
+    @blocked_keys ||= []
+    @blocked_keys << key
+    block.call
+    @blocked_keys.delete(key)
+  end
+
   def self.run_job(job)
     job.delay.run
   end
 
   class JobProcessor < Struct.new(:concept, :interface)
     include Miletus::NamespaceHelper
+    extend Forwardable
+
+    def_delegator 'SruRifcsLookupObserver.instance', :prevent_loop
 
     def run
       identifiers = \
@@ -39,31 +59,41 @@ class SruRifcsLookupObserver < ActiveRecord::Observer
       end
       return nil if xml.nil?
 
-      facet = Miletus::Merge::Facet.find_existing(xml)
-      if facet.nil?
-        facet = concept.facets.create(:metadata => xml)
-      else
-        facet.metadata = xml
-      end
-
-      if facet.concept == concept
+      # Create or update facet
+      prevent_loop(Miletus::Merge::Facet.global_key(xml)) do
+        facet = save_facet(xml)
+        # Import related objects
         facet.reindex_concept
-        new_related_keys = concept.indexed_attributes.where(
+        new_related_keys = facet.concept.indexed_attributes.where(
           :key => 'relatedKey').pluck(:value) - related_keys
-        new_related_keys.each do |key|
-          xml = interface.lookup_by_identifier(key)
-          next if xml.nil?
-          facet = Miletus::Merge::Facet.find_existing(xml)
-          if facet.nil?
-            related_concept = Miletus::Merge::Concept.find_existing(xml)
-            related_concept ||= Miletus::Merge::Concept.create()
-            related_concept.facets.create(:metadata => xml)
-          else
-            facet.metadata = xml
-          end
+        new_related_keys.each { |k| import_related_object(k) }
+      end
+    end
+
+    private
+
+    def save_facet(xml)
+      facet = Miletus::Merge::Facet.find_existing(xml)
+      facet ||= concept.facets.new
+      facet.metadata = xml
+      facet.save!
+      facet.reindex_concept
+      facet
+    end
+
+    def import_related_object(key)
+      xml = interface.lookup_by_identifier(key)
+      return if xml.nil?
+      prevent_loop(Miletus::Merge::Facet.global_key(xml)) do
+        facet = Miletus::Merge::Facet.find_existing(xml)
+        if facet.nil?
+          related_concept = Miletus::Merge::Concept.find_existing(xml)
+          related_concept ||= Miletus::Merge::Concept.create()
+          related_concept.facets.create(:metadata => xml)
+        else
+          facet.metadata = xml
         end
       end
-
     end
 
   end
