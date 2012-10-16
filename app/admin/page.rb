88c2aa17468ff959ace0b8740b08616f2be0cbcd
Tim Dettrick <t.dettrick@uq.edu.au>
2012-10-17 09:05:55 +1000

    Adding preview of Markdown rendering for pages.

--- app/admin/page.rb
+++ app/admin/page.rb
@@ -1,3 +1,19 @@
 ActiveAdmin.register Page do
 
+  show do
+    attributes_table do
+      row :id
+      row :name
+      row "Raw Content" do |page|
+        pre page.content, :class => 'prettyprint'
+      end
+      row 'Rendered Content' do |page|
+        page.to_html.html_safe
+      end
+      row :created_at
+      row :updated_at
+    end
+    active_admin_comments
+  end
+
 end
--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -1,1 +1,5 @@
 //= require active_admin/base
+//= require google-code-prettify-rails/prettify
+
+// Pretty print on page load
+$(function() { prettyPrint() });
