31fa28e6df6ba498b8d688c9486750b6b48b74d1
Tim Dettrick <t.dettrick@uq.edu.au>
2012-10-16 17:04:50 +1000

    Adding some better looking formatting for static pages.

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -3,10 +3,11 @@ class PageController < ApplicationController
   def view
     page = Page.find_by_name(params[:name])
     if page.nil?
-      @page_name = params[:name]
+      @name = params[:name]
       render :status => 404, :layout => true, :template => 'page/not_found'
     else
-      render :status => 200, :layout => true, :text => page.to_html
+      @html = page.to_html.html_safe
+      render :status => 200, :layout => true
     end
   end
 
