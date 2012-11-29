ddf3e44abe57c09c7060eb912225329def57dc0a
Tim Dettrick <t.dettrick@uq.edu.au>
2012-11-29 12:34:02 +1000

    Adding more tests for SEO controller.

--- spec/controllers/seo_controller_spec.rb
+++ spec/controllers/seo_controller_spec.rb
@@ -22,17 +22,53 @@ describe SeoController do
 
   describe "GET 'siteindex'" do
 
+    subject do
+      get 'siteindex'
+      response.should be_success
+      response
+    end
+
+    matcher :be_valid_siteindex do
+      description { "be a valid sitemap index" }
+      match do |response|
+        extend Miletus::NamespaceHelper
+        doc = Nokogiri::XML(response.body)
+        ns_by_prefix('siteindex').schema.validate(doc) == []
+      end
+    end
+
     it "should be provided by seo#robots" do
       { :get => "/siteindex.xml" }.should route_to(
         :controller => 'seo',
         :action => 'siteindex')
     end
 
-    it "should be a valid sitemap index" do
-      get 'siteindex'
-      response.should be_success
-      doc = Nokogiri::XML(response.body)
-      ns_by_prefix('siteindex').schema.validate(doc).should == []
+    context "when no concepts or pages exist" do
+      it { should be_valid_siteindex }
+    end
+
+    context "when only pages exist" do
+      before(:each) do
+        Page.create(:name => 'about', :content => '## About')
+        Page.create(:name => 'index', :content => '## Index')
+      end
+      it { should be_valid_siteindex }
+    end
+
+    context "when only concepts exist" do
+      before(:each) do
+        Miletus::Merge::Concept.create()
+      end
+      it { should be_valid_siteindex }
+    end
+
+    context "when pages and concepts exist" do
+      before(:each) do
+        Page.create(:name => 'about', :content => '## About')
+        Page.create(:name => 'index', :content => '## Index')
+        Miletus::Merge::Concept.create()
+      end
+      it { should be_valid_siteindex }
     end
 
   end
